b912744eb027c9e3b05fbf6e0da7a7d2fb9f3b7d
zt <907237461@qq.com>
2017-08-19 22:15:32 +0800

    delete file

--- node_modules/.bin/crc32.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\crc32\bin\runner.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\crc32\bin\runner.js" %*
-)
